36e2303d83e073fbbb9510b8f3973e6b0fcc1843
Kaosphere <alex.jumeline@gmail.com>
2012-05-16 11:49:31 -0400

    add emacs and htop. add login as a parameter used for chsh

--- installConf.sh
+++ installConf.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+if [ $# -ne 1 ]; then 
+    echo "enter your system login name as a parameter"
+    exit 1
+fi
 
 apt-get install -y terminator
 apt-get install -y gedit
+apt-get install -y htop
+apt-get install -y emacs
 apt-get install -y git-core gitk git-gui
 mkdir ~/.config
 cd ~/.config
 git clone git://github.com/ctaf42/ctafconf.git
 ./ctafconf/bin/ct-installconf
 apt-get install -y zsh
-chsh -s /bin/zsh psycko
+chsh -s /bin/zsh $1
 
