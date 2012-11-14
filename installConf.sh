15fc8db6df959e3b06ed35257875b76125005953
kaosphere <alex.jumeline@gmail.com>
2012-11-14 12:07:10 -0500

    change installation order so that the chsh is effective

--- installConf.sh
+++ installConf.sh
@@ -6,9 +6,9 @@ apt-get install -y htop
 apt-get install -y emacs
 apt-get install -y git-core gitk git-gui
 apt-get install -y gawk
+apt-get install -y zsh
+chsh -s /bin/zsh $USER
 mkdir ~/.config
 cd ~/.config
 git clone git://github.com/ctaf42/ctafconf.git
 ./ctafconf/bin/ct-installconf
-apt-get install -y zsh
-chsh -s /bin/zsh $USER
